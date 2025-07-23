af02fc42c03fb2d7e9f63a4228c3df1becc2bd7c
khuongnguyen <ntkhuong.coder@gmail.com>
2025-07-24 01:40:45 +0700

    add script help to install depend packages
    Signed-off-by: khuongnguyen <ntkhuong.coder@gmail.com>

---
+++ env-setup.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install libglib2.0-dev-bin
+sudo apt install python3-pip
+sudo apt install python3-gi gir1.2-gtk-3.0 libgdk-pixbuf2.0-dev
+sudo apt install -y python3-gi gir1.2-gtk-3.0 libgdk-pixbuf2.0-dev python3-opencv
+sudo apt install python3-opencv
+sudo apt install python3-opencv
