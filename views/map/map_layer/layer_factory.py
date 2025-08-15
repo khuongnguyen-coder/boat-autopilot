302ca7405f08c39614ed2a483abbacc197b23030
khuongnguyen <khuong.tan@cavliwireless.com>
2025-08-16 05:30:49 +0700

    layer: add support ACHBRT layer
    ACHBRT usually represents Depth Areas / Contours in nautical charts
    
    Signed-off-by: khuongnguyen <khuong.tan@cavliwireless.com>

--- views/map/map_layer/layer_factory.py
+++ views/map/map_layer/layer_factory.py
@@ -1,4 +1,5 @@
 from views.map.map_layer.achare_layer import ACHARELayer
+from views.map.map_layer.achbrt_layer import ACHBRTLayer
 
 LAYER_CLASS_MAP = {
     "ACHARE": {
@@ -7,5 +8,14 @@ LAYER_CLASS_MAP = {
         "width": 1,
         "fill_color": (0.5, 1.0, 1.0),      # Light cyan fill
         "fill_opacity": 0.1                 # Semi-transparent
+    },
+
+    "ACHBRT": {
+        "class": ACHBRTLayer,
+        "line_color": (0.0, 0.0, 0.5),      # Dark blue for depth contour lines
+        "width": 1,
+        "fill_color": (0.7, 0.85, 1.0),     # Light blue for shallow water areas
+        "fill_opacity": 0.3                  # Semi-transparent to show map underneath
     }
+
 }
